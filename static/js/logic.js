2691633d4b81debcbfc4bf0f5a7fa01848b4259b
Jonnie Tyler <no reply>
2018-10-20 15:16:12 -0700

    added fault lines to overlayer control

--- static/js/logic.js
+++ static/js/logic.js
@@ -88,60 +88,65 @@ function createMap(earthquakes) {
     "Outdoors" : outdoormap
   };
 
-  var overlayMaps = {
-    Earthquakes: earthquakes
-  };
+  
 
    // Create our map, giving it the satellite map, other base maps and earthquakes layer to display on load
    var tyMap = L.map("map", {
     center: [15.5994, -28.6731],
-    zoom: 3,
+    zoom: 2.75,
     layers: [satellitemap, earthquakes]
   });
 
   d3.json(techUrl, function(d){
-    L.geoJSON(d, {
+    var faultlines = L.geoJSON(d, {
       color: "yellow",
       stroke: true}).addTo(tyMap)
-    });
 
-  // Create a layer control
-  // Pass in our baseMaps and overlayMaps
-  // Add the layer control to the map
-  L.control.layers(baseMaps, overlayMaps, {
-    collapsed: false
-  }).addTo(tyMap); 
+      var overlayMaps = {
+        Earthquakes: earthquakes,
+        Faultlines: faultlines
+      };
+      
+      // Create a layer control
+    // Pass in our baseMaps and overlayMaps
+    // Add the layer control to the map
+    L.control.layers(baseMaps, overlayMaps, {
+      collapsed: false
+    }).addTo(tyMap); 
 
-  function getColor(d) {
-    return d > 5 ? '#d73027' :
-      d > 4  ? '#fc8d59' :
-      d > 3  ? '#fee08b' :
-      d > 2  ? '#d9ef8b' :
-      d > 1  ? '#91cf60' :
-      d > 0  ? '#1a9850' :
-                '#0000';
-    }
-        
-  var legend = L.control({position: 'bottomright'});
+    function getColor(d) {
+      return d > 5 ? '#d73027' :
+        d > 4  ? '#fc8d59' :
+        d > 3  ? '#fee08b' :
+        d > 2  ? '#d9ef8b' :
+        d > 1  ? '#91cf60' :
+        d > 0  ? '#1a9850' :
+                  '#0000';
+      }
+          
+    var legend = L.control({position: 'bottomright'});
 
-  legend.onAdd = function (map) {
+    legend.onAdd = function (map) {
+      
+      var div = L.DomUtil.create('div', 'info legend'),
+      grades = [0, 1, 2, 3, 4, 5],
+      labels = [];
+
+      // loop through our density intervals and generate a label with a colored square for each interval
+      for (var i = 0; i < grades.length; i++) {
+          div.innerHTML +=
+              '<i style="background:' + getColor(grades[i] + 1) + '"></i> ' +
+              grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+      }
+
+      return div;
+      };
+      
+
+    legend.addTo(tyMap);
+
+  });
     
-    var div = L.DomUtil.create('div', 'info legend'),
-    grades = [0, 1, 2, 3, 4, 5],
-    labels = [];
-
-    // loop through our density intervals and generate a label with a colored square for each interval
-    for (var i = 0; i < grades.length; i++) {
-        div.innerHTML +=
-            '<i style="background:' + getColor(grades[i] + 1) + '"></i> ' +
-            grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
-    }
-
-    return div;
-    };
     
-
-  legend.addTo(tyMap);
-
 
 }
